f2445e97bea22f60793d4ed65df0d673ee9bbaf1
이종은 <69974163+LeeJongEun02@users.noreply.github.com>
2025-02-01 21:07:08 +0900

    Create baekjoon_10869.java
    - 백준 10869번 문제
    - 두 수를 입력 받고 사칙연산 한 결과를 출력하는 코드

---
+++ baekjoon/baekjoon_10869.java
@@ -0,0 +1,27 @@
+/**
+ * 백준 10869번 문제
+ * 브론즈 5 / 사칙연산
+ * 두 수를 입력받고 사칙연산을 한 결과를 출력하는 문제
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
